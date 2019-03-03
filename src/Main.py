034e8dd2f91940900f84f7862456c943f14856f4
Ehsan <sehsanm@gmail.com>
2019-03-03 18:22:00 +0400

    Saving PPMI matrix Better Performance for  analogy

--- src/Main.py
+++ src/Main.py
@@ -1,28 +1,35 @@
+import evaluation
 import numpy as np
 import os
 import re
 
 from scipy import sparse
-from scipy.sparse.linalg import  svds
+from scipy.sparse.linalg import svds
 
-from src import evaluation
+from src.evaluation import print_closest
 
 SPLIT_RE = re.compile('( |\\!|,|:|\\[|\\]|<|>|\\{|\\}|%|\\$|\\^|\\&|\\*|\\.|\\?|\\"|\\~|\\+|=،)+')
 
 
 def load_lil_from_file(tmp_file, main_matrix):
     print('Loading pre-processed matrix : ' + tmp_file)
+    cnt = 0
     with open(tmp_file, 'r', encoding='UTF-8') as c_file:
         line = c_file.readline()
         while line:
             line = line.strip('\t\r\n ')
             parts = line.split(',')
             if len(parts) == 3:
-                main_matrix[int(parts[0]),int(parts[1])] = float(parts[2])
+                main_matrix[int(parts[0]), int(parts[1])] = float(parts[2])
+                cnt = cnt + 1
+            if cnt % 1000000 == 0:
+                print('Processed ', cnt)
             line = c_file.readline()
+
 
 def save_lil_to_file(tmp_file, main_matrix):
     print('Saving processed matrix : ' + tmp_file)
+    cnt = 0
     with open(tmp_file, 'w', encoding='UTF-8') as c_file:
         nnz_row, nnz_col = main_matrix.nonzero()
 
@@ -30,7 +37,9 @@ def save_lil_to_file(tmp_file, main_matrix):
             i = nnz_row[ind]
             j = nnz_col[ind]
             c_file.write(str(i) + ',' + str(j) + ',' + str(main_matrix[i, j]) + '\n')
-
+            cnt = cnt + 1
+            if cnt % 1000000 == 0:
+                print(cnt)
 
 def load_corpus(corpus_file, index_dict, window_size=5):
     """This file will load the corpus file and creates the
@@ -48,30 +57,38 @@ def load_corpus(corpus_file, index_dict, window_size=5):
             line = line.strip('\t\r\n ')
             line_cnt = line_cnt + 1
             if line_cnt % 10000 == 0:
-                print('Lines processed: %s  : %s' , line_cnt,  line)
+                print('Lines processed: %s  : %s', line_cnt, line)
             if len(line) != 0:
 
                 tokens = split_line(line)
                 last_n = []
                 for t in tokens:
                     if len(t) > 0 and t in index_dict:
+                        col_indexes = set()
                         for prev_token in last_n:
-                            main_matrix[index_dict[t], index_dict[prev_token]] = main_matrix[index_dict[t], index_dict[
-                                prev_token]] + 1
-
+                            col_indexes.add(index_dict[prev_token])
+                        col_indexes_sort = list(col_indexes)
+                        ll = main_matrix[index_dict[t] , col_indexes_sort]
+                        for i in range(len(ll)):
+                            ll[i] = ll[i] + 1
+                        main_matrix[index_dict[t] , col_indexes_sort] = ll
                         last_n.append(t)
                         if len(last_n) > window_size:
                             last_n.pop(0)
             line = c_file.readline()
 
-    ret =  main_matrix + main_matrix.T
+    ret = main_matrix + main_matrix.T
 
-    save_lil_to_file(tmp_file , ret)
+    save_lil_to_file(tmp_file, ret)
     return ret
 
-def ppmi_inplace(matrix, k = 5 ):
-    """Calculates the PPMI of the matrix and replace the matrix inplace"""
 
+def ppmi_inplace(matrix, k=5):
+    """Calculates the PPMI of the matrix and replace the matrix inplace"""
+    file_name = tmp_base + '-ppmi-' + str(k) + '.txt'
+    if os.path.isfile(file_name):
+        print('Loading ppmi from file ')
+        load_lil_from_file(file_name, matrix)
     print('Calculating PPMI')
     row_sum = matrix.sum(0)
     col_sum = matrix.sum(1)
@@ -85,8 +102,9 @@ def ppmi_inplace(matrix, k = 5 ):
         ppmi = max(0, np.log(matrix[i, j] * all_sum / (row_sum[0, i] * col_sum[j, 0])) - k)
         matrix[i, j] = ppmi
 
-    return matrix
+    save_lil_to_file(file_name, matrix)
 
+    return matrix
 
 def calculate_svd(matrix, dim=300):
     print('Calculating SVDs')
@@ -122,10 +140,15 @@ def build_vocab_index_from_corpus(corpus_input_file, high_pass, low_pass):
 
     print('Build Index from Corpus')
     vocab_freq = {}
+    line_cnt = 0
     with open(corpus_input_file, 'r', encoding='UTF-8') as input:
         line = input.readline()
+
         while line:
             line = line.strip('\t\r\n ')
+            line_cnt = line_cnt + 1
+            if line_cnt % 10000 == 0:
+                print('Line ', line_cnt)
             if len(line) > 0:
                 tokens = split_line(line)
                 for t in tokens:
@@ -139,9 +162,9 @@ def build_vocab_index_from_corpus(corpus_input_file, high_pass, low_pass):
     lst = list()
     for v, freq in vocab_freq.items():
         add = True
-        if high_pass != None and freq > high_pass:
+        if high_pass is not None and freq > high_pass:
             add = False
-        if low_pass != None and  freq < low_pass:
+        if low_pass is not None and freq < low_pass:
             add = False
         if add:
             lst.append(v)
@@ -160,43 +183,45 @@ def split_line(line):
 
 def build_model(corpus_file, index_file, matrix_file, min_freq, window_size):
     if not os.path.isfile(index_file):
-        index = build_vocab_index_from_corpus(corpus_file, None,  min_freq)
+        index = build_vocab_index_from_corpus(corpus_file, None, min_freq)
         write_list_to_file(index, index_file)
     index_dict, rev_model = load_vocab_index_from_file(index_file)
-    matrix = load_corpus(corpus_file, index_dict , window_size)
-    ppmi_inplace(matrix)
+    matrix = load_corpus(corpus_file, index_dict, window_size)
+    #matrix = sparse.lil_matrix((len(index_dict), len(index_dict)))
+    ppmi_inplace(matrix , k=0)
     print('shape matrix is ')
     print(matrix.shape)
     u, s, vt = calculate_svd(matrix)
-    np.savetxt(matrix_file, u * s)
+    np.savetxt(matrix_file, u * s * s)
 
 
-def test_model(index_file , matrix_file):
+def test_model(index_file, matrix_file):
     data_set = evaluation.load_analogy('../data/analogy.csv')
     index_dict, rev_index = load_vocab_index_from_file(index_file)
     print('Loading Matrix')
     embedding_matrix = np.loadtxt(matrix_file)
 
     evaluation.run_analogy(embedding_matrix, index_dict, data_set, rev_index)
-    test_lst = ['آمریکا' , 'ظریف' , 'خانه' ,'پول' , 'ایران' ]
+    test_lst = ['آمریکا', 'ظریف', 'خانه', 'پول', 'ایران']
+    top_n = 100
     for t in test_lst:
         if t in index_dict:
-            knn = evaluation.find_knn(embedding_matrix[index_dict[t]][:] , embedding_matrix)
             print(t)
-            for x in range(20):
-                print('\t' + rev_index[knn[x]])
+            vec = embedding_matrix[index_dict[t]][:]
+            print_closest(vec, embedding_matrix, rev_index, top_n)
+
 
 if __name__ == "__main__":
-    #base = 'sample'
-    #base = 'bijan_khan_ut'
-    base='wiki'
+    # base = 'sample'
+    # base = 'bijan_khan_ut'
+    base = 'wiki'
     param_window_size = 10
     param_k = 5
-    param_min_freq = 100
+    param_min_freq = 10
     tmp_base = '../data/' + base
-    param_index_file =  tmp_base+ '-index.txt'
+    param_index_file = tmp_base + '-index.txt'
     param_matrix_file = tmp_base + '-vector.txt'
-    param_corpus_file = tmp_base +  '.txt'
+    param_corpus_file = tmp_base + '.txt'
 
-    #build_model(param_corpus_file, param_index_file, param_matrix_file,  param_min_freq , param_window_size)
+    build_model(param_corpus_file, param_index_file, param_matrix_file, param_min_freq, param_window_size)
     test_model(param_index_file, param_matrix_file)
--- src/evaluation.py
+++ src/evaluation.py
@@ -1,5 +1,6 @@
 import scipy
 import numpy as np
+from numpy import random
 
 from scipy.spatial.distance import cosine
 
@@ -18,25 +19,22 @@ def load_analogy(file_name):
     return dataset
 
 
-def check_analogy(embedding_matrix, rev_index , word_1, word_2, word_3, answer_index, tol=10):
+def sort_similarity(embedding_matrix, rev_index, word_1, word_2, word_3, answer_index):
     """Check the analogy """
     knn = find_knn(embedding_matrix[word_3][:] + embedding_matrix[word_2][:] - embedding_matrix[word_1][:]
-                   , embedding_matrix)
-    if answer_index in knn[0:tol]:
-        return True
-    else:
-        # print(rev_index[word_1] + ':' + rev_index[word_2] + ':' + rev_index[word_3])
-        # for ind in knn[0:tol]:
-        #     print('\t' + rev_index[ind])
-        return False
+                   , embedding_matrix , 'c')
+    return np.where(knn == answer_index)[0][0]
 
 
-def run_analogy(embedding_matrix, index_dictionary, analogy_dataset, rev_index):
+def run_analogy(embedding_matrix, index_dictionary, analogy_dataset, rev_index , tol = 10):
     print('Running Analogy Test')
     correct_cat = {'total': 0}
     skipped_cat = {'total': 0}
     total_cat = {'total': 0}
-    for item in analogy_dataset:
+    rand_ind = list(range(len(analogy_dataset)))
+    random.shuffle(rand_ind)
+    for r_ind in rand_ind:
+        item = analogy_dataset[r_ind]
         if not item[0] in correct_cat:
             correct_cat[item[0]] = 0
         if not item[0] in skipped_cat:
@@ -47,25 +45,42 @@ def run_analogy(embedding_matrix, index_dictionary, analogy_dataset, rev_index):
         total_cat[item[0]] = total_cat[item[0]] + 1
         total_cat['total'] = total_cat['total'] + 1
 
-        if item[1] in index_dictionary and \
-                item[2] in index_dictionary and \
-                item[3] in index_dictionary and \
-                item[4] in index_dictionary:
+        found = True
+        if not item[1] in index_dictionary:
+            found = False
+            print(item[1], 'Not found')
+        if not item[2] in index_dictionary:
+            found = False
+            print(item[2], 'Not found')
+        if not item[3] in index_dictionary:
+            found = False
+            print(item[3], 'Not found')
+        if not item[4] in index_dictionary:
+            found = False
+            print(item[4], 'Not found')
+        if found:
             w_1_ind = index_dictionary[item[1]]
             w_2_ind = index_dictionary[item[2]]
             w_3_ind = index_dictionary[item[3]]
             w_4_ind = index_dictionary[item[4]]
 
-            if check_analogy(embedding_matrix, rev_index,  w_1_ind, w_2_ind, w_3_ind, w_4_ind):
+
+            loc =  sort_similarity(embedding_matrix, rev_index, w_1_ind, w_2_ind, w_3_ind, w_4_ind)
+            if loc < tol:
                 print('Perfect ' + item[1] + ' to ' + item[2] + ' is like ' + item[3] + ' to ' + item[4])
                 correct_cat[item[0]] = correct_cat[item[0]] + 1
                 correct_cat['total'] = correct_cat['total'] + 1
+            else:
+                print('Failed for: ' +  item[1] + ' to ' + item[2] + ' is like ' + item[3] + ' to ' + item[4], ', AnswerIndex:' , loc)
 
 
 
         else:
+
             skipped_cat['total'] = skipped_cat['total'] + 1
             skipped_cat[item[0]] = skipped_cat[item[0]] + 1
+
+        print('Accuracy so far:', correct_cat['total'] / total_cat['total'])
 
     print(total_cat)
     print(correct_cat)
@@ -75,15 +90,26 @@ def cosine_dist(word_vec, embed_vec):
     return cosine(word_vec, embed_vec)
 
 
+def euclid_dist(word_vec, embed_vec):
+    return np.linalg.norm(word_vec - embed_vec)
+
+
 def find_knn(word_vec, embedding_matrix, distance_method='c'):
     """Each row of the embedding matrix is the vector representation of words"""
 
     dists = []
 
-    for row in range(len(embedding_matrix)):
-        if distance_method == 'c':
-            dists.append(cosine_dist(word_vec, embedding_matrix[row][:]))
+
+    if distance_method == 'c':
+        dists = 1 - np.dot(embedding_matrix , word_vec) / (np.linalg.norm(embedding_matrix , axis=1)  * np.linalg.norm(word_vec))
+
     return np.argsort(dists)
 
 if __name__ == "__main__":
     pass
+
+
+def print_closest(vec, embedding_matrix, rev_index, top_n):
+    knn = find_knn(vec, embedding_matrix , 'e')
+    for x in range(top_n):
+        print('\t' + rev_index[knn[x]], '\t', cosine_dist(vec, embedding_matrix[knn[x]][:]))
